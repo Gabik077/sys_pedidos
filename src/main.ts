6577cae1676d4b0ce052f7c29aff27e047beb2e9
Gabriel Cabrera <gcabrera@posibillian.tech>
2025-08-05 13:06:18 -0300

    setGlobalPrefix and transform added

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import * as compression from 'compression';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.use(compression());
   app.enableCors({
     origin: ['http://localhost:3000', 'https://syspedidos.gabidev.tech'],
-    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATH',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
+  if (process.env.NODE_ENV !== 'production') {
+    app.useGlobalPipes(new ValidationPipe({
+      // whitelist: true,
+      //   forbidNonWhitelisted: true,
+      transform: true,
+    }));
+  }
+
+  app.setGlobalPrefix('api');
 
   await app.listen(4000);
 }
