c34c3fce807e45d63a984b0b484dae28c6231b68
Gabriel Cabrera <gcabrera@posibillian.tech>
2025-11-06 17:42:40 -0300

    readme , main edited

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(compression());
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://syspedidos.gabidev.tech'],
+    origin: ['http://localhost:3000'],
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
